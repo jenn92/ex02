f01fcee54ab96014deafbaa93b394175d81a843c
Jennifer Cote <jennifer.cote@hotmail.com>
2014-02-02 20:58:37 -0500

    initial version

---
+++ bin/ex02_2.dart
@@ -0,0 +1,6 @@
+void main()  {
+  var pi = 3.14159265359;
+  var r = 10;
+  var a = (r*r)*pi;
+  print ("a = (r*r)*pi = $a");
+  }
---
+++ bin/ex02_4.dart
@@ -0,0 +1,7 @@
+void main() {
+  double seconde = 9.58;
+  var metre = 100;
+  var vitesse = metre / seconde;
+  print ("vitesse = metre / seconde : $vitesse");
+
+}
